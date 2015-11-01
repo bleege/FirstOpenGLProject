130b560365fa8601ed03e4233e20bcae87058201
Brad Leege <bleege@gmail.com>
2015-10-31 21:11:47 -0500

    Setting up GLSurfaceView and checking for OpenGL ES 2 support

--- app/src/main/java/com/firstopenglproject/android/firstopenglproject/MainActivity.java
+++ app/src/main/java/com/firstopenglproject/android/firstopenglproject/MainActivity.java
@@ -1,13 +1,34 @@
 package com.firstopenglproject.android.firstopenglproject;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Setup GLSurfaceView
+        glSurfaceView = new GLSurfaceView(this);
+
+        // Check for OpenGL ES 2 Support
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        // Everything after || is for checking if running on emulator
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000 ||
+                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 &&
+                (Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith(" unknown") ||
+                Build.MODEL.contains("google_sdk") || Build.MODEL.contains("Emulator") ||
+                Build.MODEL.contains("Android SDK built for x86")));
+
     }
 }
